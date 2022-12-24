7a2a7340733e2e0d5013c11ae3d28a3fc244dec2
Petro Sobchuk <sobchuk.p@gmail.com>
2022-12-24 20:03:54 +0200

    lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,71 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = buildGallery(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
 
-console.log(galleryItems);
+/**
+  |============================
+  | Будуємо галерею
+  |============================
+*/
+function buildGallery(galleryItems) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+
+  return markup;
+}
+
+/**
+  |============================
+  | Обробляємо кліки
+  |============================
+*/
+gallery.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+    <img src="${evt.target.dataset.source}"
+    width="800"
+    height="600">
+    `,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", closeModalWindow);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", closeModalWindow);
+      },
+    }
+  );
+
+  instance.show();
+
+  function closeModalWindow(evt) {
+    if (evt.code !== "Escape") {
+      return;
+    }
+
+    return instance.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = buildGallery(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
 
-console.log(galleryItems);
+/**
+  |============================
+  | Будуємо галерею
+  |============================
+*/
+function buildGallery(galleryItems) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+
+  return markup;
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+lightbox.on("show.simplelightbox");
